88661fd4e0a78493d75e59ff870673ff0d0b6573
Temesgen Adane <tedenadane@gmail.com>
2025-07-26 13:58:09 -0700

    fix email link

---
+++ src/components/CopyButton.svelte
@@ -0,0 +1,21 @@
+<script>
+  export let text = "";
+  let copied = false;
+
+  async function copyToClipboard() {
+    await navigator.clipboard.writeText(text);
+    copied = true;
+    setTimeout(() => (copied = false), 2000);
+  }
+</script>
+
+<button
+  on:click={copyToClipboard}
+  class="text-sm px-2 py-0 rounded border dark:border-linen-white/70 dark:hover:border-brand-teal transition"
+>
+  {#if copied}
+    Copied!
+  {:else}
+    Copy
+  {/if}
+</button>
